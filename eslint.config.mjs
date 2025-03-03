2977eff8a45a82f63b8841a96380ed719aa8e44d
Kevin Eger <egerkevinjames@gmail.com>
2025-03-03 11:06:18 -0800

    chore: allow unbound methods in test (#33)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -65,6 +65,12 @@ export default tseslint.config(
       "@typescript-eslint/unbound-method": "off",
     },
   },
+  {
+    files: ["**/*.unit.test.ts"],
+    rules: {
+      "@typescript-eslint/unbound-method": "off",
+    },
+  },
   // Intentionally last to override any conflicting rules.
   eslintConfigPrettier,
 );
