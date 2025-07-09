943187b10c9b1a7cd70c5366f48c0792442dc565
Ashley Toney <ashleytoney@google.com>
2025-07-09 14:22:09 -0500

    test: move notebook creation into describe block

--- src/test/extension.e2e.test.ts
+++ src/test/extension.e2e.test.ts
@@ -24,105 +24,109 @@ describe("Colab Extension", function () {
     workbench = new Workbench();
     driver = workbench.getDriver();
     await driver.sleep(2000);
-
-    // Create a notebook that we can execute. Note that it's created with a
-    // single code cell by default.
-    await workbench.executeCommand("Create: New Jupyter Notebook");
-    await workbench.executeCommand("Notebook: Edit Cell");
-    const cell = await driver.switchTo().activeElement();
-    await cell.sendKeys("1 + 1");
   });
 
-  it("executes the notebook", async () => {
-    // Select the Colab server provider from the kernel selector.
-    await workbench.executeCommand("Notebook: Select Notebook Kernel");
-    let inputBox = await InputBox.create();
-    await inputBox.selectQuickPick("Select Another Kernel...");
-    inputBox = await InputBox.create();
-    await inputBox.selectQuickPick("Colab");
+  describe("with a notebook", () => {
+    beforeEach(async () => {
+      // Create an executable notebook. Note that it's created with a single
+      // code cell by default.
+      await workbench.executeCommand("Create: New Jupyter Notebook");
+      await workbench.executeCommand("Notebook: Edit Cell");
+      const cell = await driver.switchTo().activeElement();
+      await cell.sendKeys("1 + 1");
+    });
 
-    // Accept the dialog allowing the Colab extension to sign in using Google.
-    let dialog = new ModalDialog();
-    await dialog.pushButton("Allow");
+    it("executes the notebook", async () => {
+      // Select the Colab server provider from the kernel selector.
+      await workbench.executeCommand("Notebook: Select Notebook Kernel");
+      let inputBox = await InputBox.create();
+      await inputBox.selectQuickPick("Select Another Kernel...");
+      inputBox = await InputBox.create();
+      await inputBox.selectQuickPick("Colab");
 
-    // Begin the sign-in process by copying the OAuth URL to the clipboard and
-    // opening it in a browser window. Why do this instead of triggering the
-    // "Open" button in the dialog? We copy the URL so that we can use a new
-    // driver instance for the OAuth flow, since the original driver instance
-    // does not have a handle to the spawned window.
-    dialog = new ModalDialog();
-    await dialog.pushButton("Copy");
-    const clipboardy = await import("clipboardy");
-    const oauthUrl = clipboardy.default.readSync();
-    const oauthDriver = await new Builder().forBrowser("chrome").build();
-    await oauthDriver.get(oauthUrl);
+      // Accept the dialog allowing the Colab extension to sign in using Google.
+      let dialog = new ModalDialog();
+      await dialog.pushButton("Allow");
 
-    // Input the test account email address.
-    const emailInput = await oauthDriver.findElement(
-      By.css("input[type='email']"),
-    );
-    await emailInput.sendKeys(process.env.TEST_ACCOUNT_EMAIL ?? "");
-    await emailInput.sendKeys(Key.ENTER);
+      // Begin the sign-in process by copying the OAuth URL to the clipboard and
+      // opening it in a browser window. Why do this instead of triggering the
+      // "Open" button in the dialog? We copy the URL so that we can use a new
+      // driver instance for the OAuth flow, since the original driver instance
+      // does not have a handle to the spawned window.
+      dialog = new ModalDialog();
+      await dialog.pushButton("Copy");
+      const clipboardy = await import("clipboardy");
+      const oauthUrl = clipboardy.default.readSync();
+      const oauthDriver = await new Builder().forBrowser("chrome").build();
+      await oauthDriver.get(oauthUrl);
 
-    // Input the test account password. Note that we wait for the page to settle
-    // to avoid getting stale element reference
-    await oauthDriver.wait(
-      until.urlContains("accounts.google.com/v3/signin/challenge"),
-      ELEMENT_WAIT_MS,
-    );
-    await oauthDriver.sleep(1000);
-    const passwordInput = await oauthDriver.findElement(
-      By.css("input[type='password']"),
-    );
-    await passwordInput.sendKeys(process.env.TEST_ACCOUNT_PASSWORD ?? "");
-    await passwordInput.sendKeys(Key.ENTER);
+      // Input the test account email address.
+      const emailInput = await oauthDriver.findElement(
+        By.css("input[type='email']"),
+      );
+      await emailInput.sendKeys(process.env.TEST_ACCOUNT_EMAIL ?? "");
+      await emailInput.sendKeys(Key.ENTER);
 
-    // Click Continue to sign in to Colab.
-    await oauthDriver.wait(
-      until.urlContains("accounts.google.com/signin/oauth/id"),
-      ELEMENT_WAIT_MS,
-    );
-    let continueButton = await oauthDriver.findElement(
-      By.xpath("//span[text()='Continue']"),
-    );
-    await continueButton.click();
+      // Input the test account password. Note that we wait for the page to
+      // settle to avoid getting stale element reference
+      await oauthDriver.wait(
+        until.urlContains("accounts.google.com/v3/signin/challenge"),
+        ELEMENT_WAIT_MS,
+      );
+      await oauthDriver.sleep(1000);
+      const passwordInput = await oauthDriver.findElement(
+        By.css("input[type='password']"),
+      );
+      await passwordInput.sendKeys(process.env.TEST_ACCOUNT_PASSWORD ?? "");
+      await passwordInput.sendKeys(Key.ENTER);
 
-    // Click Continue to allow the Colab extension to access the account.
-    await oauthDriver.wait(
-      until.urlContains("accounts.google.com/signin/oauth/v2/consent"),
-      ELEMENT_WAIT_MS,
-    );
-    continueButton = await oauthDriver.findElement(
-      By.xpath("//span[text()='Continue']"),
-    );
-    await continueButton.click();
+      // Click Continue to sign in to Colab.
+      await oauthDriver.wait(
+        until.urlContains("accounts.google.com/signin/oauth/id"),
+        ELEMENT_WAIT_MS,
+      );
+      let continueButton = await oauthDriver.findElement(
+        By.xpath("//span[text()='Continue']"),
+      );
+      await continueButton.click();
 
-    // The test account should be authenticated. Close the browser window.
-    await oauthDriver.wait(until.urlContains("127.0.0.1"), ELEMENT_WAIT_MS);
-    await oauthDriver.quit();
+      // Click Continue to allow the Colab extension to access the account.
+      await oauthDriver.wait(
+        until.urlContains("accounts.google.com/signin/oauth/v2/consent"),
+        ELEMENT_WAIT_MS,
+      );
+      continueButton = await oauthDriver.findElement(
+        By.xpath("//span[text()='Continue']"),
+      );
+      await continueButton.click();
 
-    // Now that we're authenticated, we can resume creating a Colab server via
-    // the open kernel selector.
-    inputBox = await InputBox.create();
-    await inputBox.selectQuickPick("New Colab Server");
-    // Select the variant.
-    inputBox = await InputBox.create();
-    await inputBox.selectQuickPick("CPU");
-    // Alias the server with the default name.
-    inputBox = await InputBox.create();
-    await inputBox.sendKeys(Key.ENTER);
-    // Select the kernel.
-    inputBox = await InputBox.create();
-    await inputBox.selectQuickPick("Python 3 (ipykernel)");
+      // The test account should be authenticated. Close the browser window.
+      await oauthDriver.wait(until.urlContains("127.0.0.1"), ELEMENT_WAIT_MS);
+      await oauthDriver.quit();
 
-    // Execute the notebook and poll for the success indicator (green check).
-    // Why not the cell output? Because the output is rendered in a webview.
-    await workbench.executeCommand("Notebook: Run All");
-    await driver.wait(async () => {
-      const element = await workbench
-        .getEnclosingElement()
-        .findElements(By.className("codicon-notebook-state-success"));
-      return element.length > 0;
-    }, ELEMENT_WAIT_MS);
+      // Now that we're authenticated, we can resume creating a Colab server via
+      // the open kernel selector.
+      inputBox = await InputBox.create();
+      await inputBox.selectQuickPick("New Colab Server");
+      // Select the variant.
+      inputBox = await InputBox.create();
+      await inputBox.selectQuickPick("CPU");
+      // Alias the server with the default name.
+      inputBox = await InputBox.create();
+      await inputBox.sendKeys(Key.ENTER);
+      // Select the kernel.
+      inputBox = await InputBox.create();
+      await inputBox.selectQuickPick("Python 3 (ipykernel)");
+
+      // Execute the notebook and poll for the success indicator (green check).
+      // Why not the cell output? Because the output is rendered in a webview.
+      await workbench.executeCommand("Notebook: Run All");
+      await driver.wait(async () => {
+        const element = await workbench
+          .getEnclosingElement()
+          .findElements(By.className("codicon-notebook-state-success"));
+        return element.length > 0;
+      }, ELEMENT_WAIT_MS);
+    });
   });
 });
