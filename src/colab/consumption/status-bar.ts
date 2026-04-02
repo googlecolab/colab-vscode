3278cc08186bbcea9b1aa48fcf33c436a822490f
Jack Yang <hjjackyang@google.com>
2026-04-01 17:55:23 -0700

    docs: add TODO

--- src/colab/consumption/status-bar.ts
+++ src/colab/consumption/status-bar.ts
@@ -35,6 +35,8 @@ export class ConsumptionStatusBar implements Toggleable, Disposable {
     this.consumptionListener = onDidChangeCcuInfo((e) => {
       this.updateStatusBarItem(e);
     });
+    // TODO: Triggers a CCU pull on server assignment changes.
+    // Better to be implemented in ConsumptionPoller.
   }
 
   /**
